68cdc5f12e9513a95394e221188007882d59b489
Chernika656 <levkutepov2008@gmail.com>
2025-02-21 10:20:10 +0300

    Delete tg_dressBot directory

--- tg_dressBot/dresswebapp/shopapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-       path('', name='index'), # Добавили шаблон для корневого URL
-       path('add_to_cart/', views.add_to_cart, name='add_to_cart'),
-   ]
-
